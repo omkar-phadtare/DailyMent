590a1bf7e1651df9f8ee708419998c863ac3e7fc
omkar_phadtare <op8775@gmail.com>
2023-03-04 13:29:43 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,13 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
 
+  app.use(express.urlencoded({extended:false}));
+  app.use(
+    cors({
+        origin:["http://localhost:5000","https://daily-ment.onrender.com"],
+    })
+  )
+
 app.listen(port,function(){
     
     console.log("Server is running on port ",{port});
